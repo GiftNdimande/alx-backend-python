e75d3bdea0c3ab1b5c2f7307d4744152c58423e8
GiftNdimande <sfisotsitso@gmail.com>
2023-10-08 00:01:26 +0000

    annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Truncate float to integer
+"""
+def floor(n: float) -> int:
+    """
+        Args:
+            n: float number
+        Return:
+            Float number truncated to floor
+    """
+    return int(n)
